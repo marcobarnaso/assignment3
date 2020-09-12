6554a15f6208bfcd6f502abb87831036b78ffbc1
mrojas <kdashcr@gmail.com>
2020-09-12 09:37:40 -0600

    adding callback to assignment 3 and complete assignment 4

--- assignment-3.js
+++ assignment-3.js
@@ -1,12 +1,14 @@
+const chalk = require("chalk")
+
 const sellComputer = ({ quantity }, sold) => {
     try {
         if (quantity < sold) {
-            throw new Error(`There is not enough stock, current stock is ${quantity}`)
-        } else if(sold < 1) {
-            throw new Error('Quantity sold must be 1 or greater')
+            throw new Error(chalk.red.inverse(`There is not enough stock, current stock is ${quantity}`))
+        } else if (sold < 1) {
+            throw new Error(chalk.red.inverse('Quantity sold must be 1 or greater'))
         }
         quantity -= sold
-        console.log(`Computers sold: ${sold}, stock remaining: ${quantity}`)
+        console.log(chalk.blue.inverse(`Computers sold: ${sold}, stock remaining: ${quantity}`))
 
     } catch (error) {
         console.log(error.message)
@@ -15,25 +17,55 @@ const sellComputer = ({ quantity }, sold) => {
 }
 
 const getDetails = ({ vendor, model, price }) => {
-    console.log(`${vendor} ${model}, for price: ${price}`)
+    console.log(chalk.blue.inverse(`${vendor} ${model}, for price: ${price}`))
 }
 
-const performanceTest = async ({ performanceTests }, date) => {
-    const passed = Math.random() < 0.7;
-
-    performanceTests.push(`Date: ${date}, Passed: ${passed}`)
-
-    console.log('Performing test...')
+const performanceTest = ({ performanceTests }, date, callback) => {
+    console.log(chalk.yellow.inverse('Performing test...'))
 
     setTimeout(() => {
-        console.log('Test Finished')
-        console.log(performanceTests)
-    }, 2000)
+        const testResult = Math.random() < 0.7;
+        performanceTests.push(`Date: ${date}, Passed: ${testResult}`)
+        const testMessage = 'Test finished'
+        callback(undefined, { performanceTests, testMessage })
+
+    }, 1000)
 }
+
+const performanceTestPromise = ({ performanceTests }, date) => {
+    console.log(chalk.yellow.inverse('Performing test...'))
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const testResult = Math.random() < 0.7;
+            performanceTests.push(`Date: ${date}, Passed: ${testResult}`)
+            const testMessage = 'Test finished'
+            resolve({ pt: performanceTests, tm: testMessage })
+        }, 2000)
+    })
+}
+
+async function wait(ms) {
+    
+}
+
+const performanceTestAsyncAwait = async ({ performanceTests }, date) => {
+    console.log(chalk.yellow.inverse('Performing test...'))
+    
+    await new Promise(resolve => {
+        setTimeout(resolve, 1000)
+    })
+        const testResult = Math.random() < 0.7;
+        performanceTests.push(`Date: ${date}, Passed: ${testResult}`)
+        const testMessage = 'Test finished'
+        return ({ pt: performanceTests, tm: testMessage })
+    }
 
 module.exports = {
     sellComputer,
     getDetails,
-    performanceTest
+    performanceTest,
+    performanceTestPromise,
+    performanceTestAsyncAwait
 }
 
--- playground.js
+++ playground.js
@@ -1,6 +1,9 @@
+const chalk = require('chalk')
 const { sellComputer } = require('./assignment-3')
 const { getDetails } = require('./assignment-3')
 const { performanceTest } = require('./assignment-3')
+const { performanceTestPromise } = require('./assignment-3')
+const { performanceTestAsyncAwait } = require('./assignment-3')
 
 const computer = {
     vendor: 'Asus',
@@ -14,4 +17,28 @@ sellComputer(computer, 1)
 
 getDetails(computer)
 
-performanceTest(computer, '09/10/2020')
+// performanceTest(computer, '09/10/2020', (error, {performanceTests, testMessage}) => {
+//     console.log(chalk.green.inverse(performanceTests[0]))
+//     console.log(chalk.yellow.inverse(testMessage))
+// })
+
+
+// performanceTestPromise(computer, '09/10/2020')
+// .then(data => {
+//     if(!data) {
+//         throw new Error('Something went wrong')
+//     }
+
+//     console.log(chalk.green.inverse(data.pt[0]))
+//     console.log(chalk.yellow.inverse(data.tm))
+// }).catch((error) => console.log(error.message))
+
+performanceTestAsyncAwait(computer, '09/11/2020')
+.then(data => {
+    console.log(chalk.green.inverse(data.pt[0]))
+    console.log(chalk.yellow.inverse(data.tm))
+}).catch((e) => {
+    console.log(e)
+})
+
+
